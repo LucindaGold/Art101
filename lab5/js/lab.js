bd7f4d6272cfdfc096aca4c7dcc2e947f399fa67
LucindaGold <lurgold@gmail.com>
2021-01-27 18:58:46 -0800

    push

---
+++ lab5/js/lab.js
@@ -0,0 +1,35 @@
+console.log("I am linked!");
+
+
+let currentYear = 2021;
+//Define Kai's Car
+let user1 = "Kai";
+let make1 = "Fiat";
+let model1 = "500 Abarth";
+let color1 = "White w/ red accents";
+let year1 = 2013;
+
+let age1 = currentYear - year1;
+
+//Define Lucinda's Car
+let user2 = "Lucinda";
+let make2 = "Volvo";
+let model2 = "480";
+let color2 = "Navy Blue";
+let year2 = 1989;
+
+let age2 = currentYear - year2;
+
+document.writeln("User: " + user1 +"<br>");
+document.writeln("Make: " + make1 +"<br>");
+document.writeln("Model: " + model1 +"<br>");
+document.writeln("Color: " + color1 +"<br>");
+document.writeln("Year: " + year1 +"<br>");
+document.writeln("Age: " + age1 +"<br><br>");
+
+document.writeln("User: " + user2 +"<br>");
+document.writeln("Make: " + make2 +"<br>");
+document.writeln("Model: " + model2 +"<br>");
+document.writeln("Color: " + color2 +"<br>");
+document.writeln("Year: " + year2 +"<br>");
+document.writeln("Age: " + age2 +"<br>");
